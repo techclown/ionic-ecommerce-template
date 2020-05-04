e1b4bf6c0f44d540aefa16bec35b99863641965d
Holumide <ajayimide420@gmail.com>
2020-05-04 15:25:58 +0100

    Added the home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,6 +28,10 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 
 ];
